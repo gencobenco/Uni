c63a3c81f49868fa0b32c5d0351aabcc0b213f18
GencoBenco <anion_cheddar_0p@icloud.com>
2024-02-14 13:11:27 +0100

    main

--- Powershell/Powershell Basics.ps1
+++
@@ -1,8 +0,0 @@
-$farbe = Read-Host "Welche Farbe hat das Stopp-Schild?"
-
-if($farbe -eq "rot") {
-    Write-Host "Rot ist die richtige Farbe."
-}
-Else {
-    Write-Host "Falsch. $farbe ist es nicht."
-}
